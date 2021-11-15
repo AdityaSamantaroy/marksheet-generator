c4f25157ac1a1927a8526f406b551885e35a00dc
mayank64ce <mayank_1901ee72@iitp.ac.in>
2021-11-15 13:28:26 +0530

    Added upload to backend functionality

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+# setup flask application
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'tronlives4ever'
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
